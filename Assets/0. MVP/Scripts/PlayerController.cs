63e193f6e3c64aeddda7e25d2f8202ce5c954ac8
Albytrosss <AlbatrossGames@pm.me>
2025-10-11 11:50:39 +1100

    Recreated player controller in new Unity Editor version (6.2)

--- Assets/0. MVP/Scripts/PlayerController.cs
+++ Assets/0. MVP/Scripts/PlayerController.cs
@@ -1,16 +1,57 @@
+using System;
+using NUnit.Framework.Internal;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputActionAsset InputActions;
     
-    void Start()
+    private InputAction iMoveAction;
+    private InputAction iJumpAction;
+
+    private Vector2 moveAmount;
+    private Rigidbody rb;
+    
+    public float walkSpeed = 5;
+    public float jumpSpeed = 5;
+    private void OnEnable()
     {
-        Debug.Log("test");
+        InputActions.FindActionMap("Player").Enable();
+    }
+    private void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
     }
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        iMoveAction = InputActions.FindAction("Player/Move");
+        iJumpAction = InputActions.FindAction("Player/Jump");
+        rb = GetComponent<Rigidbody>();
+    }
     void Update()
     {
-        
+        moveAmount = iMoveAction.ReadValue<Vector2>();
+        print(moveAmount);
+        if (iJumpAction.WasPressedThisFrame())
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.AddForceAtPosition(new Vector3(0, jumpSpeed, 0), Vector3.up, ForceMode.Impulse);
+    }
+
+    private void FixedUpdate()
+    {
+        Walking();
+    }
+
+    private void Walking()
+    {
+        rb.MovePosition(rb.position + transform.forward * moveAmount.x * walkSpeed * Time.deltaTime);
     }
 }
