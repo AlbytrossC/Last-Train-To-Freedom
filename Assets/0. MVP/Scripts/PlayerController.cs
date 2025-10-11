12b3a31d1d8353000dbe3a14fd5e65c03befc346
Albytrosss <AlbatrossGames@pm.me>
2025-10-11 18:17:31 +1100

    started climbing mechanic

--- Assets/0. MVP/Scripts/PlayerController.cs
+++ Assets/0. MVP/Scripts/PlayerController.cs
@@ -8,10 +8,13 @@ public class PlayerController : MonoBehaviour
     public InputActionAsset InputActions;
     private InputAction iMoveAction;
     private InputAction iJumpAction;
+    private InputAction iRunAction;
+    
     private Vector2 moveAmount;
     private Rigidbody rb;
     public float walkSpeed = 50;
-    public float jumpForce = 50;
+    public float jumpForce = 40;
+    public float climbSpeed = 50;
     private bool isGrounded = true;
     private void OnEnable()
     {
@@ -26,6 +29,7 @@ public class PlayerController : MonoBehaviour
     {
         iMoveAction = InputActions.FindAction("Player/Move");
         iJumpAction = InputActions.FindAction("Player/Jump");
+        iRunAction = InputActions.FindAction("Player/Run");
         rb = GetComponent<Rigidbody>();
     }
     void Update()
@@ -52,10 +56,15 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Walking();
+        Climbing();
     }
 
     private void Walking()
     {
         rb.MovePosition(rb.position + transform.forward * moveAmount.x * walkSpeed * Time.deltaTime);
+    }
+    private void Climbing()
+    {
+        rb.AddForce(transform.up * moveAmount.y * climbSpeed * Time.deltaTime, ForceMode.Force);
     }
 }
